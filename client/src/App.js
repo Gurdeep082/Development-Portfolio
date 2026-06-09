f73e6e3bd47020272fc04d1f0192c0f90524835c
Gurdeep <sainigurdeep082@gmail.com>
2026-06-09 17:40:41 +0530

    admin hash route

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,9 @@ import Admin from "./pages/Admin";
 import Home from "./pages/Home";
 
 function App() {
-  return window.location.pathname.startsWith("/admin") ? <Admin /> : <Home />;
+  return window.location.hash === "#admin"
+    ? <Admin />
+    : <Home />;
 }
 
 export default App;
