82afe301bce84b78cc2ecd013b6c563827153a11
Gurdeep <sainigurdeep082@gmail.com>
2026-06-09 20:23:04 +0530

    admin hash route

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Admin from "./pages/Admin";
 import Home from "./pages/Home";
 
 function App() {
-  return window.location.hash === "#admin"
-    ? <Admin />
-    : <Home />;
+  const [isAdmin, setIsAdmin] = useState(
+    window.location.hash === "#admin"
+  );
+
+  useEffect(() => {
+    const onHashChange = () => {
+      setIsAdmin(window.location.hash === "#admin");
+    };
+
+    window.addEventListener("hashchange", onHashChange);
+    return () => window.removeEventListener("hashchange", onHashChange);
+  }, []);
+
+  return isAdmin ? <Admin /> : <Home />;
 }
 
 export default App;
