618bbaf69d236b5cba3f7eeb68771e4ebecb1b5f
Gurdeep <sainigurdeep082@gmail.com>
2026-06-09 21:13:44 +0530

    icons added

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,14 @@
 import React, { useEffect, useState } from "react";
 import Admin from "./pages/Admin";
 import Home from "./pages/Home";
+import LoadingScreen from "./components/LoadingScreen";
 
 function App() {
   const [isAdmin, setIsAdmin] = useState(
     window.location.hash === "#admin"
   );
+
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     const onHashChange = () => {
@@ -13,8 +16,27 @@ function App() {
     };
 
     window.addEventListener("hashchange", onHashChange);
-    return () => window.removeEventListener("hashchange", onHashChange);
+
+    const isPWA =
+      window.matchMedia("(display-mode: standalone)").matches ||
+      window.navigator.standalone === true;
+
+    if (isPWA) {
+      setLoading(true);
+
+      setTimeout(() => {
+        setLoading(false);
+      }, 2000);
+    }
+
+    return () => {
+      window.removeEventListener("hashchange", onHashChange);
+    };
   }, []);
+
+  if (loading) {
+    return <LoadingScreen />;
+  }
 
   return isAdmin ? <Admin /> : <Home />;
 }
---
+++ client/src/components/Loadingscreen.jsx
@@ -0,0 +1,16 @@
+import "./Loadingscreen.css";
+export default function LoadingScreen() {
+  return (
+    <div className="loading-screen">
+      <div className="spinner"></div>
+
+      <div className="loading-title">
+        Gurdeep Portfolio
+      </div>
+
+      <div className="loading-text">
+        Loading...
+      </div>
+    </div>
+  );
+}
